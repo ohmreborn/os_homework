1ab18ff1b2fcc6841a9f4ef83124bba0cb8563b0
ohm <krittiwitkampradam@gmail.com>
2025-10-31 14:26:53 +0700

    simple factor

--- 1_parallel_6610501963/include/config.h
+++ 1_parallel_6610501963/include/config.h
@@ -1,14 +1,11 @@
 #include <stdint.h>
 
-int number_test_size = 6;
-uint64_t numbers[6] = {
+int number_test_size = 3;
+uint64_t numbers[3] = {
 	(uint64_t)1<<10,
 	(uint64_t)1<<20,
 	(uint64_t)1<<30,
-	(uint64_t)1<<40,
-	(uint64_t)1<<50,
-	(uint64_t)1<<60,
 };
 int thread_test_size = 4;
 int num_threads[4] = {1, 2, 4, 8};
-int num_benchmark = 2;
+int num_benchmark = 1000;
--- 1_parallel_6610501963/parallel.c
+++ 1_parallel_6610501963/parallel.c
@@ -10,27 +10,36 @@
 
 #include <config.h>
 
-void find_factor_pairs(uint64_t number, uint64_t sqrt_n, bool *is_factor) {
-	#pragma omp parallel for schedule(static)
-	for (uint64_t i = 1; i <= sqrt_n; i++) {
-		if (number % i == 0){
-			is_factor[i - 1] = true;
+// Padded structure to prevent false sharing
+// Each element occupies a full cache line (64 bytes)
+typedef struct {
+	bool value;
+	char padding[63];  // 1 + 63 = 64 bytes (typical cache line size)
+} padded_bool;
+
+void find_factor_pairs(uint64_t number, uint64_t sqrt_n, int thread_size, padded_bool *is_factor) {
+	omp_set_num_threads(thread_size);
+#pragma omp parallel 
+	{
+		int thread_id = omp_get_thread_num();
+		for (uint64_t i = 1+thread_id; i <= sqrt_n; i+=thread_size) {
+			is_factor[i-1].value = (number % i == 0);
 		}
 	}
 }
 
-double bench_mark(uint64_t number, uint64_t sqrt_n, bool *is_factor){
-	double start = omp_get_wtime();
-	find_factor_pairs(number, sqrt_n, is_factor);
-	double end = omp_get_wtime();
-//	printf("Total time: %.4f s\n", end - start);
-	return end - start;
-
+double bench_mark(uint64_t number, uint64_t sqrt_n, int thread_size, padded_bool *is_factor){
+	clock_t start, end;
+	start = clock();
+	find_factor_pairs(number, sqrt_n, thread_size, is_factor);
+	end = clock();
+	double cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+	return cpu_time_used;
 }
 
-void show(bool *is_factor, uint64_t sqrt_n, uint64_t number){
+void show(padded_bool *is_factor, uint64_t sqrt_n, uint64_t number){
 	for (uint64_t i=0;i<sqrt_n;i++){
-		if (is_factor[i]){
+		if (is_factor[i].value){
 			printf("%" PRIu64 ", " "%" PRIu64 "\n", i+1, number/(i+1) );
 		}
 	}
@@ -43,7 +52,6 @@ int main() {
 		fprintf(fptr, "%d,", num_threads[i]);
 	}
 	fprintf(fptr, "\n");
-	
 
 	for (int test_num_i=0; test_num_i<number_test_size; test_num_i++){
 		fprintf(fptr, "%" PRIu64 ",", numbers[test_num_i]);
@@ -52,16 +60,16 @@ int main() {
 			int thread_size = num_threads[test_thread_i];
 
 			uint64_t sqrt_n = (uint64_t)sqrt((double)number);
-			size_t alloc_size = sizeof(bool) * sqrt_n;
-			bool *is_factor = (bool *)malloc(alloc_size);
-			printf("Prime factorization of %" PRIu64 "with num_threads %d" ": ", number, thread_size);
+			size_t alloc_size = sizeof(padded_bool) * sqrt_n;
+			padded_bool *is_factor = (padded_bool *)malloc(alloc_size);
+			printf("Prime factorization of %" PRIu64 " with num_threads %d: ", number, thread_size);
 			double average = 0;
 			double SD = 0;
 			for (int i=0;i<num_benchmark;i++){
-				result[i] = bench_mark(number, sqrt_n, is_factor);
+				result[i] = bench_mark(number, sqrt_n, thread_size, is_factor);
 				average += result[i];
-//				show(is_factor, sqrt_n, number);
-				memset(is_factor, false, alloc_size); 
+				//show(is_factor, sqrt_n, number);
+				memset(is_factor, 0, alloc_size); 
 			}
 			average /= (double)num_benchmark;
 			for (int i=0;i<num_benchmark;i++){
@@ -72,12 +80,11 @@ int main() {
 			fprintf(fptr, "%f,", average);
 			free(is_factor);
 		}
-		printf("\n");
 		fprintf(fptr, "\n");
 	}	
 
 	free(result);
 	fclose(fptr);
+
 	return 0;
 }
-
