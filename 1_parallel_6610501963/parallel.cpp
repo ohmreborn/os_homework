889a564b7e9dc897a306e517a53c2c3e971f7a50
ohm <krittiwitkampradam@gmail.com>
2025-11-01 23:01:35 +0700

    add serial and parallel part

--- 1_parallel_6610501963/parallel.cpp
+++ 1_parallel_6610501963/parallel.cpp
@@ -28,13 +28,19 @@ void find_factor_pairs(uint64_t number, uint64_t sqrt_n, int thread_size, std::v
 	}
 }
 
-double bench_mark(uint64_t number, uint64_t sqrt_n, int thread_size, std::vector<uint64_t> &factor){
+void bench_mark(uint64_t number, int thread_size, std::vector<double> &result_serial, std::vector<double> &result_parallel, int i){
 	clock_t start, end;
+
+	start = clock();
+	uint64_t sqrt_n = (uint64_t)sqrt((double)number);
+	std::vector<uint64_t> factor;
+	end = clock();
+	result_serial[i] = ((double) (end - start)) / CLOCKS_PER_SEC;
+
 	start = clock();
 	find_factor_pairs(number, sqrt_n, thread_size, factor);
 	end = clock();
-	double cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-	return cpu_time_used;
+	result_parallel[i] = ((double) (end - start)) / CLOCKS_PER_SEC;
 }
 
 void show(std::vector<uint64_t> &factor, uint64_t number){
@@ -45,46 +51,58 @@ void show(std::vector<uint64_t> &factor, uint64_t number){
 }
 
 int main() {
-	std::vector<uint64_t> factor;
-	std::vector<double> result(num_benchmark,0);
-	std::ofstream output("result.csv"); 
-	if (!output.is_open()) {
+	std::vector<double> result_serial(num_benchmark,0);
+	std::vector<double> result_parallel(num_benchmark,0);
+	std::ofstream serial("result_serial.csv"); 
+	if (!serial.is_open()) {
 		std::cout << "Error: cannot open file.\n"; 
 		return 1; // Indicate an error
 	}
+	std::ofstream parallel("result_parallel.csv"); 
 	for (int i=0;i<thread_test_size;i++){
-		output << num_threads[i] << ",";
+		serial << num_threads[i] << ",";
+		parallel << num_threads[i] << ",";
 	}
-	output << '\n';
+	serial << '\n';
+	parallel << '\n';
 
 	for (int test_num_i=0; test_num_i<number_test_size; test_num_i++){
-		output << numbers[test_num_i] << ',';
+		serial << numbers[test_num_i] << ',';
+		parallel << numbers[test_num_i] << ',';
 		for (int test_thread_i=0; test_thread_i<thread_test_size; test_thread_i++){
 			uint64_t number = numbers[test_num_i];
 			int thread_size = num_threads[test_thread_i];
 
-			uint64_t sqrt_n = (uint64_t)sqrt((double)number);
 			std::cout << "Prime factorization of " << number << " with num_threads: " << thread_size << '\n';
-			double average = 0;
-			double SD = 0;
+			double average_serial = 0;
+			double SD_serial = 0;
+			double average_parallel = 0;
+			double SD_parallel = 0;
 			for (int i=0;i<num_benchmark;i++){
-				result[i] = bench_mark(number, sqrt_n, thread_size, factor);
-				average += result[i];
+				bench_mark(number, thread_size, result_serial, result_parallel, i);
+				average_serial += result_serial[i];
+				average_parallel += result_parallel[i];
 				// show(factor, number);
-				factor.clear();
 			}
-			average /= (double)num_benchmark;
+			average_serial /= (double)num_benchmark;
+			average_parallel /= (double)num_benchmark;
 			for (int i=0;i<num_benchmark;i++){
-				SD += (average - result[i]) * (average - result[i]);
+				SD_serial += (average_serial - result_serial[i]) * (average_serial - result_serial[i]);
+				SD_parallel += (average_parallel - result_parallel[i]) * (average_parallel - result_parallel[i]);
 			}
-			SD = sqrt(SD) / (double)num_benchmark;
-			std::cout << average << "±" << SD << '\n';
-			output << average << ',';
+			SD_serial = sqrt(SD_serial) / (double)num_benchmark;
+			SD_parallel = sqrt(SD_parallel) / (double)num_benchmark;
+			std::cout << "Serial: " << average_serial << "±" << SD_serial << '\n';
+			std::cout << "Parallel: " << average_parallel << "±" << SD_parallel << '\n';
+			serial << average_serial << ',';
+			parallel << average_parallel << ',';
 		}
-		output << '\n';
+		serial << '\n';
+		parallel << '\n';
 	}	
 
-	output.close();
+	serial.close();
+	parallel.close();
 
 	return 0;
 }
