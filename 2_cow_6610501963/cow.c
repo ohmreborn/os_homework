09f681998f5109b5cae04d84e95c1c6609599535
ohm <krittiwitkampradam@gmail.com>
2025-11-02 15:17:13 +0700

    fix wait procress

--- 2_cow_6610501963/cow.c
+++ 2_cow_6610501963/cow.c
@@ -1,12 +1,14 @@
 #include <stdio.h> 
 #include <stdlib.h> 
 #include <unistd.h> // fork
+#include <sys/wait.h> // wait
 #include <sys/resource.h> // get rusage
 
 void show_mem(){ 
 	struct rusage mem_info; 
 	getrusage(RUSAGE_SELF, &mem_info); 
-	int mem = mem_info.ru_maxrss / (1024 * 1024);
+	// On Linux, ru_maxrss is in kilobytes, not bytes
+	int mem = mem_info.ru_maxrss / 1024;  // Convert KB to MB
 	printf("%d MB\n", mem); 
 }
 
@@ -17,11 +19,11 @@ int main(int argc, char *argv[]){
 	} else {
 		memsize = 50 * 1024 * 1024; // default 50 MB
 	}
-	printf("before allocate memory\n");
+	printf("before allocate memory: ");
 	show_mem(); 
 
 	if (memsize % sizeof(int) != 0){
-		printf("memsize is doesn't fit in sizeof int\n");
+		printf("memsize doesn't fit in sizeof int\n");
 		return 1;
 	}
 	int num_element = memsize / sizeof(int);
@@ -30,32 +32,43 @@ int main(int argc, char *argv[]){
 		printf("cannot use malloc\n");
 		return 1;
 	}
-	for (int i=0;i< num_element;i++){ 
-		a[i] += 1; 
+	// Initialize the array to ensure pages are allocated
+	for (int i=0; i < num_element; i++){ 
+		a[i] = 1; 
 	}
+
+	printf("after allocate and write memory: ");
+	show_mem();
 
 	pid_t child_id = fork();
 	if (child_id < 0){
 		free(a);
-		printf("cannot fork child procress \n");
+		printf("cannot fork child process\n");
 		return 1;
 	}
-	if (child_id == 0){ // child procress
+	if (child_id == 0){ // child process
 		printf("child use before write a memory: ");
 		show_mem(); 
-		for (int i=0;i<num_element;i++){
+
+		// Write to the array to trigger COW
+		for (int i=0; i < num_element; i++){
 			a[i] = 2;
 		}
+
 		printf("child use after write a memory: ");
 		show_mem(); 
-	}else{ // parent procress
-		for (int i=0;i<num_element;i++){
-			a[i] = 1;
+		exit(0);
+	} else { // parent process
+			 // Write to the array in parent
+		for (int i=0; i < num_element; i++){
+			a[i] = 3;
 		}
 		printf("parent use: ");
 		show_mem(); 
+
 		int status;
 		wait(&status);
+		printf("child exit status: %d\n", status);
 	}
 
 	free(a); 
